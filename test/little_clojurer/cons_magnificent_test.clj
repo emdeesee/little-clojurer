13a1c22625473b75628bbdfc45b9509fa49adb7c
Michael Cornelius <kouneriasu@gmail.com>
2015-08-03 16:22:33 -0500

    some rember tests

---
+++ test/little_clojurer/cons_magnificent_test.clj
@@ -0,0 +1,25 @@
+(ns little-clojurer.cons-magnificent-test
+  (:require [clojure.test :refer :all]
+            [little-clojurer.cons-magnificent :refer :all]))
+
+(deftest test-rember
+  
+  (let [a 'mint
+        lat '(lamb chops with mint jelly)
+        expect '(lamb chops with jelly)]
+    (is (= expect (rember a lat))))
+  
+  (let [a 'mint
+        lat '(lamb chops with mint flavored mint jelly)
+        expect '(lamb chops with flavored mint jelly)]
+    (is (= expect (rember a lat))))
+
+  (let [a 'toast
+        lat '(bacon lettuce and tomato)
+        expect lat]
+    (is (= expect (rember a lat))))
+
+  (let [a 'cup
+        lat '(coffee cup tea cup and hick cup)
+        expect '(coffee tea cup and hick cup)]
+   (is (= expect (rember a lat)))))
